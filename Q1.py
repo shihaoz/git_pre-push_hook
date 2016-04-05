a89f02308384724efa5375b917b2d0f7598823d6
DavidZhang <shihaoz@umich.edu>
2016-04-05 14:00:47 -0400

    save Q1-3

---
+++ Q1.py
@@ -0,0 +1,26 @@
+import sys
+import re #regular expression
+
+if len(sys.argv) < 4:
+  print 'usage: <search word> <minimum appearance> <file_name1> ..'
+  exit(1)
+
+# get word and min appear
+word = sys.argv[1]
+min_count = int(sys.argv[2])
+
+print 'args:', word, min_count
+
+for idx in xrange(3, len(sys.argv)):
+  file_name = sys.argv[idx]
+  ifstream = open(file_name, 'rU')
+  """
+  USE regular expression to examine the file
+  """
+  pattern = r"{0}".format(word)	# interpret as raw string
+  matches = re.findall(pattern, ifstream.read())
+
+  if len(matches) >= min_count:
+  	print file_names
+  
+  ifstream.close()
---
+++ Q2.py
@@ -0,0 +1,25 @@
+import re #regular expression
+
+
+def Searcher():
+  return _searcher
+
+class _searcher:
+  """
+  define a searcher class that
+  input: <Word>, <min_count>, <list of filename>
+  output: <list of filename that contain Word at least min_count times>
+  """
+  call_count = 0
+  def __init__(self):
+    return
+  def search(self, word, min_count, file_list):
+    pattern = r'{0}'.format(word)
+    for file_name in file_list:
+      ifstream = open(file_name, 'rU')
+      matches = re.findall(pattern, ifstream.read())
+
+      if len(matches) >= min_count:
+        print file_name
+    self.call_count += 1
+    return
